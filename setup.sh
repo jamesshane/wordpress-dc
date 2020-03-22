67cf78f1e7d44f5e5ebada2e4ad13759781e5603
James Shane <jamesshane@gmail.com>
2020-03-22 05:33:06 -0600

    full push

---
+++ setup.sh
@@ -0,0 +1,18 @@
+docker network create wordpress-network && \
+docker volume create --name mariadb_data && \
+docker run -d --name mariadb \
+  --env ALLOW_EMPTY_PASSWORD=yes \
+  --env MARIADB_USER=bn_wordpress \
+  --env MARIADB_DATABASE=bitnami_wordpress \
+  --network wordpress-network \
+  --volume mariadb_data:/bitnami/mariadb \
+  bitnami/mariadb:latest 
+docker volume create --name wordpress_data && \
+docker run -d --name wordpress \
+  -p 8080:8080 -p 8443:8443 \
+  --env ALLOW_EMPTY_PASSWORD=yes \
+  --env WORDPRESS_DATABASE_USER=bn_wordpress \
+  --env WORDPRESS_DATABASE_NAME=bitnami_wordpress \
+  --network wordpress-network \
+  --volume wordpress_data:/bitnami/wordpress \
+  bitnami/wordpress:latest
